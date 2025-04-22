6a5509659a8711affbf286be37bfef30537e2087
Yongsin Park <yscom.co.kr@gmail.com>
2025-04-22 19:23:08 +0900

    Initial commit

---
+++ YPdfViewer/MouseWheelMode.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+#pragma warning disable 1591
+
+namespace YPdfViewer
+{
+    public enum MouseWheelMode
+    {
+        PanAndZoom,
+        Pan,
+        Zoom
+    }
+}
---
+++ YPdfViewer/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("YPdfViewer")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("yscom")]
+[assembly: AssemblyProduct("yscom")]
+[assembly: AssemblyCopyright("yscom")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: CLSCompliant(true)]
+[assembly: Guid("ca63710b-8bc7-4fc5-8c39-210bacf591e8")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
